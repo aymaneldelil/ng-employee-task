60ab01ad09c90d9185b1c3ae5121102e7f2d6ec6
aymanyori <aymanyori00@gmail.com>
2023-02-16 07:09:25 +0200

    Footer/NavBar Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HNavbarModule } from './feature/h-navbar/h-navbar.module';
+import { FooterModule } from './feature/footer/footer.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HNavbarModule,
+    FooterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
