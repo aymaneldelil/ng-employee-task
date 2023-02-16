a7bb0efae9995b92016b80c90bc66a56e7f8ba16
aymanyori <aymanyori00@gmail.com>
2023-02-16 08:58:27 +0200

    DummyData

--- src/app/view/private/dashboard/dashboard.module.ts
+++ src/app/view/private/dashboard/dashboard.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AngularMaterialModule } from 'src/app/feature/angular-material/angular-material.module';
-import { SharedModule } from 'src/app/shared/shared.module';
+import { ComponentsModule } from 'src/app/shared/components/components.module';
+
 @NgModule({
   declarations: [DashboardComponent],
-imports: [CommonModule, DashboardRoutingModule, AngularMaterialModule , SharedModule],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    AngularMaterialModule,
+    ComponentsModule,
+  ],
 })
 export class DashboardModule {}
