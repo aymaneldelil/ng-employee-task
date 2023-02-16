6fd4f0217bd382d41d399dea9f85ea215cd7c3f3
aymanyori <aymanyori00@gmail.com>
2023-02-16 07:09:40 +0200

    Footer/Navbar Module

---
+++ src/app/feature/h-navbar/h-navbar.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HNavBarComponent } from './h-nav-bar/h-nav-bar.component';
+import { AngularMaterialModule } from '../angular-material/angular-material.module';
+import { RouterLink } from '@angular/router';
+
+@NgModule({
+  declarations: [HNavBarComponent],
+  imports: [
+    CommonModule,
+    AngularMaterialModule,
+    RouterLink,
+    AngularMaterialModule,
+  ],
+  exports: [HNavBarComponent],
+})
+export class HNavbarModule {}
