f2446dd6fdf91c95e26fac1e07cc493aa143c635
aymanyori <aymanyori00@gmail.com>
2023-02-16 07:05:01 +0200

    Pages routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  
+  {path:"dashboard" , loadChildren:()=>import('./view/private/dashboard/dashboard.module').then(t=>t.DashboardModule)},
+  {path:"403" , loadChildren:()=>import('./view/public/page403/page403.module').then(t=>t.Page403Module)},
+  {path:"**" , loadChildren:()=>import('./view/public/page404/page404.module').then(t=>t.Page404Module)}
 ];
 
 @NgModule({
