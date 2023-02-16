7580de884a618c8c4b380b7eaf54b081d534fce7
aymanyori <aymanyori00@gmail.com>
2023-02-16 06:48:07 +0200

    Architecture

---
+++ src/app/view/public/page403/page403.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Page403RoutingModule } from './page403-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    Page403RoutingModule
+  ]
+})
+export class Page403Module { }
