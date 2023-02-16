a23a92575161108e74b7b71b0e8522363685f5fb
aymanyori <aymanyori00@gmail.com>
2023-02-16 06:58:30 +0200

    Page 404

--- src/app/view/public/page403/page403.module.ts
+++ src/app/view/public/page403/page403.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Page403RoutingModule } from './page403-routing.module';
+import { Page403Component } from './page403/page403.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    Page403Component
+  ],
   imports: [
     CommonModule,
     Page403RoutingModule
--- src/app/view/public/page404/page404.module.ts
+++ src/app/view/public/page404/page404.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Page404RoutingModule } from './page404-routing.module';
+import { Page404Component } from './page404/page404.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    Page404Component
+  ],
   imports: [
     CommonModule,
     Page404RoutingModule
