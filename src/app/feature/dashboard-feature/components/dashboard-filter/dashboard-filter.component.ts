a3919f9dfa488b9f61889e3df057ade9bfb6e000
aymanyori <aymanyori00@gmail.com>
2023-02-17 11:49:44 +0200

    Filter

--- src/app/feature/dashboard-feature/components/dashboard-filter/dashboard-filter.component.ts
+++ src/app/feature/dashboard-feature/components/dashboard-filter/dashboard-filter.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Component, Inject, OnInit, } from '@angular/core';
 import {
-  AbstractControl,
   FormBuilder,
   FormGroup,
-  ValidationErrors,
-  ValidatorFn,
-  Validators,
+
 } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Idepartment } from 'src/app/core/interfaces/idepartment';
 import { DummydatabasesService } from 'src/app/shared/services/dummydatabases.service';
 import { PositionService } from 'src/app/shared/services/position.service';
@@ -22,73 +20,43 @@ export class DashboardFilterComponent implements OnInit {
   // FORM GROUP
   public filterForm!: FormGroup;
 
-  //
-  public departments: Idepartment[] = [];
-  public positions!: Array<string>;
-  public selectedDepartmentId!: string;
-  //-----------------------------------
 
   //----------------------------------------------------------
   constructor(
     private _fb: FormBuilder,
     private _db: DummydatabasesService,
     private _positionSVC: PositionService,
-    private _DB: DummydatabasesService
+    private _dialogRef: MatDialogRef<DashboardFilterComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: string
   ) {}
   //-------------------------------------------------------------------------------------------------------------------------------------------
 
+  searchValue:string =''
   ngOnInit(): void {
-    this.initialForm();
-    this.initialValues();
+    // this.initialValues();
   }
-  private initialForm() {
-    this.filterForm = this._fb.group({
-      firstName: [
-        null,
-        [
-          Validators.required,
-          Validators.pattern(/^[A-Za-z]+$/),
-          Validators.minLength(3),
-        ],
-      ],
-      lastName: [
-        null,
-        [
-          Validators.required,
-          Validators.pattern(/^[A-Za-z]+$/),
-          Validators.minLength(3),
-        ],
-      ],
-      gender: [null, [Validators.required]],
-      email: [
-        null,
-        [
-          Validators.required,
-          Validators.pattern(/[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
-        ],
-      ],
-      department: [null, [Validators.required]],
-      position: [null, [Validators.required]],
-    });
-  }
-  //-------------------------------------------------------------------------------------------------------------------------------------------
 
   //-------------------------------------------------------------------------------------------------------------------------------------------
-
-  private initialValues() {
-    this._DB.getDepartment().subscribe({
-      next: (n) => {
-        this.departments = n;
-      },
-    });
+  onFilter(event: any) {
+    console.log('EVEET' , event);
+    const handelEvent = event.toLowerCase();
+    this._dialogRef.close({data : handelEvent});
   }
+
+  // private initialValues() {
+  //   this._DB.getDepartment().subscribe({
+  //     next: (n) => {
+  //       this.departments = n;
+  //     },
+  //   });
+  // }
   //-------------------------------------------------------------------------------------------------------------------------------------------
 
-  public onSelectedDep(selectedDepID: string) {
-    this._positionSVC.getPositionsOfDepartment(selectedDepID).subscribe({
-      next: (data) => {
-        this.positions = data.positions;
-      },
-    });
-  }
+  // public onSelectedDep(selectedDepID: string) {
+  //   this._positionSVC.getPositionsOfDepartment(selectedDepID).subscribe({
+  //     next: (data) => {
+  //       this.positions = data.positions;
+  //     },
+  //   });
+  // }
 }
